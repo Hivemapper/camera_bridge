597cbc96c776465425b3ecfb55818e6450b77699
Mazar Farran <mazar@hivemapper.com>
2023-12-21 19:47:39 +0000

    Add space message

--- app.cpp
+++ app.cpp
@@ -124,6 +124,7 @@ static void execute_stream(LibcameraEncoder &app, VideoOptions *options)
 int main(int argc, char *argv[])
 {
   std::cout << "Welcome Test"<< std::endl;
+  std::cout << "space: " << std::filesystem::space("/media/usb0").free << std::endl;
   try
   {
     LibcameraEncoder app;
