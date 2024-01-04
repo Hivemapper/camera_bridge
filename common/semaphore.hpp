b4ee4cd711ae76d3f46e7ae14d2c91d2383ff4f5
Mazar Farran <mazar@hivemapper.com>
2024-01-04 21:51:25 +0000

    update

--- common/semaphore.hpp
+++ common/semaphore.hpp
@@ -5,31 +5,12 @@
 
 class Semaphore
 {
-    std::mutex mutex_;
-    std::condition_variable condition_;
-    unsigned long count_ = 0; // Initialized as locked.
-
 public:
     void release()
     {
-        std::lock_guard<decltype(mutex_)> lock(mutex_);
+        std::lock_guard<std::mutex> lock(mutex_);
         ++count_;
         condition_.notify_one();
-    }
-
-    bool acquire()
-    {
-        std::unique_lock<decltype(mutex_)> lock(mutex_);
-        // while(!count_) // Handle spurious wake-ups.
-        //     condition_.wait(lock);
-        using namespace std::chrono_literals;
-        condition_.wait_for(lock, 1ms);
-        if (!count_)
-        {
-            return false;
-        }
-        --count_;
-        return true;
     }
 
     bool try_acquire()
@@ -42,4 +23,9 @@ public:
         }
         return false;
     }
+
+private:
+    std::mutex mutex_;
+    std::condition_variable condition_;
+    unsigned long count_ = 0; // Initialized as locked.
 };
